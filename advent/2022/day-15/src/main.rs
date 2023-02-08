a63450214581880dd3422a5c77a9b2213f2b7672
bdferul <118205572+bdferul@users.noreply.github.com>
2023-02-08 17:08:52 +0000

    h

--- advent/2022/day-15/src/main.rs
+++ advent/2022/day-15/src/main.rs
@@ -1,48 +1,163 @@
-/// Returns the Manhattan distance between two points
-fn man_dist(a: (i64, i64), b: (i64, i64)) -> i64 {
-    i64::abs(a.0 - b.0) + i64::abs(a.1 - b.1)
+const INPUT: &str = include_str!("input.txt");
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-/// A sensor has its center position and its exclusion range
-struct Sensor {
-    position: (i64, i64),
-    range: i64,
+/// Beacon Sensor Pair
+#[derive(Debug, Clone, Copy)]
+struct BeaSenPair {
+    sensor: Point,
+    beacon: Point,
 }
 
-impl Sensor {
-    pub fn init(pos: (i64, i64), beacon: (i64, i64)) -> Sensor {
-        Self {
-            position: pos,
-            range: man_dist(beacon, pos),
+fn main() {
+    let beasens = parse_input(INPUT);
+    let sensor_ranges = beasens
+        .iter()
+        .map(|bsp| (bsp.sensor, get_manhattan(bsp.sensor, bsp.beacon)))
+        .collect::<Vec<(Point, i32)>>();
+
+    let closest_sensor_x = sensor_ranges.iter().min_by_key(|&&(s, _)| s.x).unwrap();
+    let farthest_sensor_x = beasens.iter().map(|bsp| bsp.sensor.x).max().unwrap();
+
+    let mut part1 = 0;
+    let test_y = 2000000;
+    let beacon_collums_on_test_y = beasens
+        .iter()
+        .map(|bsp| bsp.beacon)
+        .filter(|b| b.y == test_y)
+        .map(|b| b.x)
+        .collect::<Vec<i32>>();
+
+    let start = closest_sensor_x.0.x - closest_sensor_x.1;
+
+    for x in start.. {
+        let point = Point::new(x, test_y);
+        if beacon_collums_on_test_y.contains(&x) {
+            continue;
+        }
+        let mut good = true;
+
+        for ranges in &sensor_ranges {
+            let dis = get_manhattan(point, ranges.0);
+
+            if dis <= ranges.1 {
+                good = false;
+                break;
+            }
+        }
+
+        if !good {
+            part1 += 1;
+        } else if x > farthest_sensor_x {
+            break;
         }
     }
 
-    /// Returns True if point is inside this sensor's exclusion range
-    pub fn in_exclusion_range(&self, point: (i64, i64)) -> bool {
-        self.range >= man_dist(self.position, point)
+    println!("Part 1: {part1}");
+
+    let mut part2 = None;
+    let part2_range = 0..=4000000;
+
+    'out: for (sensor, range) in sensor_ranges.clone() {
+        let rim = rim(sensor, range + 1);
+
+        for p in rim {
+            if !part2_range.contains(&p.x) || !part2_range.contains(&p.y) {
+                continue;
+            }
+
+            let mut unscanned = true;
+            for (sensor, range) in &sensor_ranges {
+                if get_manhattan(p, *sensor) <= *range {
+                    unscanned = false;
+                    break;
+                }
+            }
+
+            if unscanned {
+                part2 = Some(p);
+                break 'out;
+            }
+        }
     }
+
+    let Point { x: px, y: py } = part2.unwrap();
+
+    let px = px as i128;
+    let py = py as i128;
+
+    let part2 = (px * 4000000) + py;
+
+    println!("Part 2: {part2}");
 }
 
-/// Parses the advent input and returns the list of sensors
-fn parse(input: &str) -> Vec<Sensor> {
-    let mut result = vec![];
+/// Finds the rim of points surrounding a point beyond a specified distance;
+fn rim(point: Point, distance: i32) -> Vec<Point> {
+    let mut r = vec![];
+
+    let start_x = point.x;
+    let start_y = point.y;
+
+    for i in 0..distance {
+        r.push(Point::new(start_x + i, start_y - distance + i)); // \
+        r.push(Point::new(start_x + distance - i, start_y + i)); // /
+        r.push(Point::new(start_x - i, start_y + distance - i)); // \
+        r.push(Point::new(start_x - distance + i, start_y - i)); // /
+    }
+
+    r
+}
+
+fn get_manhattan(a: Point, b: Point) -> i32 {
+    (a.x - b.x).abs() + (a.y - b.y).abs()
+}
+
+fn parse_input(input: &str) -> Vec<BeaSenPair> {
+    let mut beasens = vec![];
 
     for line in input.lines() {
-        // line: "Sensor at x=2391367, y=3787759: closest beacon is at x=2345659, y=4354867"
-        let (first, second) = line.split_once(": ").unwrap();
+        let [_,sx,_,sy,_,bx,_,by] = &parse_line(line, &["=", ",", "=", ":", "=", ",", "="])[..] else {panic!()};
 
-        let (sensor_x, sensor_y) = first[12..].split_once(", ").unwrap();
-        let sensor_x = sensor_x.parse().unwrap();
-        let sensor_y = sensor_y[2..].parse().unwrap();
+        let sensor = Point::parse(sx, sy);
+        let beacon = Point::parse(bx, by);
 
-        let (beacon_x, beacon_y) = second[23..].split_once(", ").unwrap();
-        let beacon_x = beacon_x.parse().unwrap();
-        let beacon_y = beacon_y.parse().unwrap();
-
-        result.push(Sensor::init((sensor_x, sensor_y), (beacon_x, beacon_y)));
+        beasens.push(BeaSenPair { sensor, beacon });
     }
 
-    result
+    beasens
 }
 
-fn main() {}
+fn parse_line(input: &str, delims: &[&str]) -> Vec<String> {
+    let mut r = vec![];
+    let mut line = input;
+
+    for s in delims {
+        let Some((a, b)) = line.split_once(s) else { break };
+        r.push(a.to_string());
+        line = b;
+    }
+
+    r.push(line.to_string());
+
+    r
+}
+
+impl std::fmt::Debug for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", (self.x, self.y))
+    }
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    fn parse(x: &str, y: &str) -> Self {
+        Self::new(x.parse().unwrap(), y.parse().unwrap())
+    }
+}
